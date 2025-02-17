5cdb5d0b978eb9711b7a0ec60791190ca674618b
peitalin <n6378056@gmail.com>
2025-02-17 15:12:21 +0900

    setup telemetry basics

--- node/telemetry/src/main.rs
+++ node/telemetry/src/main.rs
@@ -1,7 +1,34 @@
-use color_eyre::Result;
+mod metrics;
 
-pub fn main() -> Result<()> {
+use color_eyre::Result;
+use metrics::REQUESTS_TOTAL;
+use prometheus_exporter;
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+
     color_eyre::install()?;
-    println!("telemetry module");
+    let url = init_telemetry()?;
+    println!("Running Prometheus on: {}", url);
+
+    // TODO: replace with application level telemetry
+    // Simulate application logic - increment counter
+    loop {
+        REQUESTS_TOTAL.inc();
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+
     Ok(())
+}
+
+/// Starts the metrics server on port 9200
+pub fn init_telemetry() -> Result<String> {
+    dotenv::dotenv().ok();
+    let port = std::env::var("PROMETHEUS_PORT")
+        .unwrap_or("9090".to_string())
+        .parse().unwrap_or("9090".to_string());
+
+    let url = format!("0.0.0.0:{}", port);
+    prometheus_exporter::start(url.parse()?)?;
+    Ok(url)
 }
