714b065cde6014c7427ee55a19a3e4cedc46033b
peitalin <n6378056@gmail.com>
2025-02-20 18:15:21 +0900

    update TEE terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,14 +19,83 @@ variable "zone" {
   default     = "asia-southeast1-a"
 }
 
+variable "github_token" {
+  description = "GitHub Personal Access Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_repo" {
+  description = "GitHub repository URL (format: owner/repo)"
+  type        = string
+}
+
+variable "repo_branch" {
+  description = "Repository branch to clone"
+  type        = string
+  default     = "main"
+}
+
 locals {
   timestamp = formatdate("YYYYMMDD-hhmmss", timestamp())
-  instance_name = "tee-4-instance-${local.timestamp}"
+  instance_name = "tee-04-instance-${local.timestamp}"
+
+  # Create startup script
+  startup_script = <<-EOF
+    #!/bin/bash
+    set -euxo pipefail
+
+    # Wait for cloud-init to complete
+    cloud-init status --wait
+
+    # Install system dependencies
+    sudo apt-get install -y \
+      apt-get install -y \
+      git \
+      pkg-config \
+      libssl-dev \
+      libtss2-dev \
+      build-essential \
+      curl
+
+    # Install Rust
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source "$HOME/.cargo/env"
+
+    # Install Just
+    curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | sudo bash -s -- --to /usr/local/bin
+
+    # Clone the repository using token in URL
+    sudo mkdir -p /opt/app
+    git clone https://${var.github_token}@github.com/${var.github_repo}.git
+    sudo chown $(whoami):$(whoami) ~/1up-network
+    cd ~/1up-network
+    git checkout ${var.repo_branch}
+
+    # Run the justfile command
+    just node1
+  EOF
 }
 
 provider "google" {
   project = var.project_id
 }
+
+# Static IP address
+resource "google_compute_address" "static_ip" {
+  name = "${local.instance_name}-ip"
+  region = substr(var.zone, 0, length(var.zone)-2)
+}
+
+# GCP Confidential VM setup
+# Instance Type: c3-standard-* family
+# Operating System: containerOS, RHEL0, SLES-15-sp5, Ubuntu 22.04
+# Supported Zones: asia-southeast-1-{a,b,c}, europe-west4-{a,b}, us-central1-{a,b,c}
+#
+# For more info on supported operating systems, check out GCP supported configurations:
+# https://cloud.google.com/confidential-computing/confidential-vm/docs/supported-configurations#intel-tdx
+# Currently, TDX enabled VMs can only be created via gcloud or Rest API:
+# https://cloud.google.com/confidential-computing/confidential-vm/docs/create-a-confidential-vm-instance#gcloud
 
 resource "google_compute_instance" "tdx_instance" {
   name         = local.instance_name
@@ -35,6 +104,7 @@ resource "google_compute_instance" "tdx_instance" {
 
   boot_disk {
     auto_delete = true
+    device_name = "boot-disk-${local.timestamp}"
     initialize_params {
       image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20250112"
       size  = 10
@@ -43,7 +113,10 @@ resource "google_compute_instance" "tdx_instance" {
   }
 
   network_interface {
-    network = "default"
+    network = "default"  # Using default VPC network
+    access_config {
+      nat_ip = google_compute_address.static_ip.address
+    }
   }
 
   service_account {
@@ -68,8 +141,12 @@ resource "google_compute_instance" "tdx_instance" {
     visible_core_count = null  // Default
   }
 
+  confidential_instance_config {
+    enable_confidential_compute = true
+  }
+
   metadata = {
-    confidential_compute_type = "TDX"
+    startup-script = local.startup_script
   }
 
   tags = ["http-server", "https-server"]
