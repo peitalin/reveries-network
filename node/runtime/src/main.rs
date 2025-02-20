0743fda05e93d15eeedd885621c95aea41d8ebe9
peitalin <n6378056@gmail.com>
2025-02-21 00:14:10 +0900

    update runtime tests

--- node/runtime/src/main.rs
+++ node/runtime/src/main.rs
@@ -3,7 +3,10 @@ mod reencrypt;
 mod llm;
 mod tee_attestation;
 mod evm;
+mod test_commands;
 
+use test_commands::{Cmd, CliArgument};
+use clap::Parser;
 use llm::{
     read_agent_secrets,
     connect_to_anthropic,
@@ -30,91 +33,89 @@ use std::str::FromStr;
 async fn main() -> color_eyre::Result<()> {
 
     color_eyre::install()?;
-
 	let _ = tracing_subscriber::FmtSubscriber::builder()
 		.with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
 		.try_init();
 
+    let cmd = Cmd::parse();
 
-    // run_reencrypt_example();
-
-    let (
-        tee_attestation_quote,
-        tee_attestation_bytes
-    ) = tee_attestation::generate_tee_attestation(true)?;
-
-
-    let agent_secrets = read_agent_secrets(0);
-    // Claude
-    let (
-        claude,
-        agent
-    ) = connect_to_anthropic(
-        &agent_secrets.anthropic_api_key.unwrap(),
-        "you are a helpful assistant"
-    );
-
-    //// DeepSeek
-    let agent = connect_to_deepseek(
-        agent_secrets.deepseek_api_key.unwrap(),
-        "You are a helpful assistant"
-    ).await;
-    // let ask = &format!("Is the following data a valid TDX QuoteV4 trusted execution environment attestation?\n{:?}", tee_attestation_quote);
-
-    // let ask = "does an LLM have a soul?";
-    // println!("\nAsking: {}\nWaiting for LLM response...", ask);
-
-    // let answer = agent.prompt(ask).await?;
-    // println!("\nAnswer: {}", answer);
-
-
-
-    // Test running an EVM
-    let caller = Address::from_str("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").unwrap();
-    // AppState contains DB
-    let app_state = AppState::new(caller.as_slice());
-
-    let mut app_state = deploy_contract(
-        app_state, // DB
-        TransactionRequest {
-            // Contract bytecode (hex string)
-            bytecode: get_1upnetwork_contract_bytecode(),
-            // Transaction input (hex string)
-            calldata: "".to_string(),
-            // Sender address (hex string)
-            caller: caller,
-            // Ether value in wei
-            value: 0,
+    match cmd.argument {
+        CliArgument::TestUmbral => {
+            run_reencrypt_example()?;
         }
-    ).unwrap();
+        CliArgument::TestTee => {
+            let (
+                tee_attestation_quote,
+                tee_attestation_bytes
+            ) = tee_attestation::generate_tee_attestation(true)?;
+        }
+        CliArgument::TestLlm => {
+            let agent_secrets = read_agent_secrets(0);
+            ///// Claude
+            let (_claude, agent) = connect_to_anthropic(
+                &agent_secrets.anthropic_api_key.unwrap(),
+                "you are a helpful assistant"
+            );
+            ///// DeepSeek
+            // let agent = connect_to_deepseek(
+            //     agent_secrets.deepseek_api_key.unwrap(),
+            //     "You are a helpful assistant"
+            // ).await;
 
-    if let Some(contract) = app_state.contract_addr {
+            let ask = "does an LLM have a soul? give me a short, one-sentence answer";
+            println!("\nAsking: {}\nWaiting for LLM response...", ask);
+            let answer = agent.prompt(ask).await?;
+            println!("\nAnswer: {}", answer);
+        }
+        CliArgument::TestEvm => {
+            // Test running an EVM
+            let caller = Address::from_str("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").unwrap();
+            // AppState contains DB
+            let app_state = AppState::new(caller.as_slice());
 
-        // increment twice
-        increment_number(&mut app_state, IncrementNumberQuery { caller, contract });
-        increment_number(&mut app_state, IncrementNumberQuery { caller, contract });
+            let mut app_state = deploy_contract(
+                app_state, // DB
+                TransactionRequest {
+                    // Contract bytecode (hex string)
+                    bytecode: get_1upnetwork_contract_bytecode(),
+                    // Transaction input (hex string)
+                    calldata: "".to_string(),
+                    // Sender address (hex string)
+                    caller: caller,
+                    // Ether value in wei
+                    value: 0,
+                }
+            ).unwrap();
 
-        let current_number = query_number(
-            &mut app_state,
-            QueryNumber { caller, contract }
-        );
-        println!("\nCurrent number: {}", current_number);
+            if let Some(contract) = app_state.contract_addr {
 
-        set_number(
-            &mut app_state,
-            SetNumberQuery {
-                caller,
-                contract,
-                number: U256::from(33)
+                // increment twice
+                increment_number(&mut app_state, IncrementNumberQuery { caller, contract });
+                increment_number(&mut app_state, IncrementNumberQuery { caller, contract });
+
+                let current_number = query_number(
+                    &mut app_state,
+                    QueryNumber { caller, contract }
+                );
+                println!("\nCurrent number: {}", current_number);
+
+                set_number(
+                    &mut app_state,
+                    SetNumberQuery {
+                        caller,
+                        contract,
+                        number: U256::from(33)
+                    }
+                );
+
+                let new_number = query_number(
+                    &mut app_state,
+                    QueryNumber { caller, contract }
+                );
+
+                println!("\nNew number: {}", new_number);
             }
-        );
-
-        let new_number = query_number(
-            &mut app_state,
-            QueryNumber { caller, contract }
-        );
-
-        println!("\nNew number: {}", new_number);
+        }
     }
 
     Ok(())
